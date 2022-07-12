721387801f97a7ae0e09cea8a0f780a127a8d9aa
Raheem <mohamedraheem0555@gmail.com>
2022-07-12 20:17:38 +0530

    installed nodemon for watch the changes

--- express-project/server.js
+++ express-project/server.js
@@ -9,8 +9,16 @@ const plannets = [
   { id: 0, name: "plannet 1" },
   { id: 1, name: "plannet 2" },
   { id: 2, name: "plannet 3" },
+  { id: 3, name: "plannet 4" },
 ];
-//way of configure the route in Express.JS
+
+// Simple ExpressJS middleware
+app.use((req, res, next) => {
+  console.log(`Method ${req.method}, request URL: ${req.url}`);
+  next();
+});
+
+// Way of configure the route in Express.JS - Rout handler
 app.get("/", (req, res) => {
   res.send("Hello Im From Express Route");
 });
@@ -33,9 +41,11 @@ app.get("/getPlannet/:plannetIndex", (req, res) => {
   const plannetId = req.params.plannetIndex;
   const plannet = plannets[plannetId];
 
+  // check `plannet` is true..
   if (plannet) {
     res.status(200).json(plannet);
   } else {
+    // if it is false them set status message and send message..
     res.status(404).json({
       error: "Plannet Not found",
     });
