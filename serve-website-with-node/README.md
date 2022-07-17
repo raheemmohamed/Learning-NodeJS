ffcb10b7ccf62ff9c445092e887ec07b1ff9333f
Raheem <mohamedraheem0555@gmail.com>
2022-07-17 16:55:55 +0530

    serve static website file with nodeJS

---
+++ serve-website-with-node/README.md
@@ -0,0 +1,19 @@
+# Express JS serve static file with NodeJS
+
+In this code you can see `public` folder which is contain static files for serve with nodeJS. use following way to server static file through NodeJS.
+
+> FileName: server.js
+
+```
+/**
+ * Load static website pages *path.join(__dirname, "public")
+ */
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+```
+
+Then you can call this via below mentioned URL
+
+```
+http://localhost:3000/static/index.html
+```
---
+++ serve-website-with-node/server.js
@@ -0,0 +1,46 @@
+//import express module
+const express = require("express");
+const path = require("path");
+
+// import routes
+const carRouter = require("./routes/cars.router");
+const plannetRouter = require("./routes/plannets.router");
+const messageRouter = require("./routes/messages.router");
+
+// call express function and assign to variable
+const app = express();
+// specified port for run server
+const PORT = 3000;
+
+//simple ExpressJS middleware - 3 for check how many millisecond for complete the request
+app.use((req, res, next) => {
+  const start = Date.now();
+
+  next(); //After call Next()
+
+  // here the place where need to do some action
+  const delta = Date.now() - start;
+  console.log(
+    `${req.method}, request URL: ${req.url}, completion time: ${delta}Ms`
+  );
+});
+
+// ExpressJs middleware for check and parses incoming requests with JSON payloads
+app.use(express.json());
+
+/**
+ * Load static website pages *path.join(__dirname, "public")
+ */
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+/**
+ * load routes to a middleware function following way that will take care
+ */
+app.use(plannetRouter); // use normal url path as well which is defined in plannet.router.js
+app.use("/cars", carRouter); // either you can path relavant path "/cars/getAllCars"
+app.use(messageRouter);
+
+// Listening Server
+app.listen(PORT, () => {
+  console.log(`Listening PORT ${PORT}`);
+});
